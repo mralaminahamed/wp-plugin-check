6e592308e0cf5c437580fea0948798bc9f570670
Nilambar Sharma <nilambar@outlook.com>
2024-12-19 14:48:15 +0545

    Add unit test for missing readme header

--- tests/phpunit/tests/Checker/Checks/Plugin_Readme_Check_Tests.php
+++ tests/phpunit/tests/Checker/Checks/Plugin_Readme_Check_Tests.php
@@ -232,6 +232,22 @@ class Plugin_Readme_Check_Tests extends WP_UnitTestCase {
 		$this->assertCount( 1, wp_list_filter( $errors['readme.txt'][0][0], array( 'code' => 'outdated_tested_upto_header' ) ) );
 	}
 
+	public function test_run_with_errors_missing_readme_headers() {
+		$readme_check  = new Plugin_Readme_Check();
+		$check_context = new Check_Context( UNIT_TESTS_PLUGIN_DIR . 'test-plugin-plugin-readme-errors-upgrade-notice/load.php' );
+		$check_result  = new Check_Result( $check_context );
+
+		$readme_check->run( $check_result );
+
+		$errors = $check_result->get_errors();
+
+		$this->assertNotEmpty( $errors );
+		$this->assertArrayHasKey( 'readme.txt', $errors );
+
+		// Check for missing tested upto header.
+		$this->assertCount( 1, wp_list_filter( $errors['readme.txt'][0][0], array( 'code' => 'missing_readme_header_tested' ) ) );
+	}
+
 	public function test_run_md_with_errors() {
 		$readme_check  = new Plugin_Readme_Check();
 		$check_context = new Check_Context( UNIT_TESTS_PLUGIN_DIR . 'test-plugin-plugin-readme-md-with-errors/load.php' );
